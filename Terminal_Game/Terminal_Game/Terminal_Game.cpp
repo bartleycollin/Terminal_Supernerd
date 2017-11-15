f8c27180265285792315241bd4d552542e84aaa8
bartleycollin <bartleycollin@yahoo.com>
2017-11-15 14:48:13 -0500

    START CODING HERE
    This build can run, and has no errors.

--- Terminal_Game/Terminal_Game/Terminal_Game.cpp
+++ Terminal_Game/Terminal_Game/Terminal_Game.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "string"
+#include "time.h"
+#include "windows.h"
 
+using namespace std;
+
+
+void type(string str) {
+	int randomNum;
+	for (int i = 0; i < str.length(); i++)
+	{
+		randomNum = (rand() % 51) + 15;
+		cout << str[i];
+		Sleep(randomNum);
+	}
+	cout << endl;
+}
 
 int main()
 {
+	type("Whoa!");
+	system("pause");
     return 0;
 }
 
